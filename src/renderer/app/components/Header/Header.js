fcb22c3c3bf4a7dc9e1d0d6955787c34c44ed492
Jesus Gómez <jesus_jgb_45@hotmail.com>
2017-06-28 23:29:24 +0200

    Header Component: Init

---
+++ src/renderer/app/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Icon } from 'semantic-ui-react';
+
+import './Header.css';
+
+const HeaderComponent = () => (
+  <div className="header_component">
+    <h1 className="header">Node Script Manager</h1>
+    <Icon name='grid layout' size='large'/>
+  </div>
+);
+
+export default HeaderComponent;
