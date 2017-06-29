44a8677357d52b6d9a893bcb88bb6edcd8cc6714
evuz <jesus_jgb_45@hotmail.com>
2017-06-29 21:27:31 +0200

    Header Component: props & filter

--- src/renderer/app/components/Header/Header.jsx
+++ src/renderer/app/components/Header/Header.jsx
@@ -3,11 +3,24 @@ import { Icon } from 'semantic-ui-react';
 
 import './Header.css';
 
-const HeaderComponent = () => (
+const HeaderComponent = (props) => (
   <div className="header_component">
-    <h1 className="header">Node Script Manager</h1>
-    <Icon name='grid layout' size='large'/>
+    <div className="header">
+      <h1 className="title">
+        {props.name ? dashToCaps(props.name) : 'Node Script Manager'}
+      </h1>
+      {
+        props.description ?
+          <h4 className="subtitle">{props.description}</h4> :
+          null
+      }
+    </div>
+    <Icon name="grid layout" size="large" className="hide" />
   </div>
 );
+
+function dashToCaps(str) {
+  return str.split('-').join(' ').replace(/\b\w/g, l => l.toUpperCase())
+}
 
 export default HeaderComponent;
