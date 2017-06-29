8e5867f0f9aea4ca19a694ec2e8fa34a7bf9f77e
evuz <jesus_jgb_45@hotmail.com>
2017-06-29 14:45:20 +0200

    Integrate store

--- src/renderer/index.jsx
+++ src/renderer/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
+import configureStore from './app/configureStore';
 import registerServiceWorker from './registerServiceWorker';
 import App from './app';
 
 import 'semantic-ui-css/semantic.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
