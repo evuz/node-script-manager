68ea496a3978ac1689c514fbc0c1eaee82d9519b
Jesus Gómez <jesus_jgb_45@hotmail.com>
2017-06-29 17:12:16 +0200

    App Component: packageJson received

--- src/renderer/app/components/App/App.jsx
+++ src/renderer/app/components/App/App.jsx
@@ -1,3 +1,4 @@
+import { ipcRenderer } from 'electron';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
@@ -11,7 +12,9 @@ import './App.css';
 
 class AppComponent extends Component {
   componentWillMount() {
-
+    ipcRenderer.on('setPackageJson', (event, data) => {
+      console.log(data);
+    })
   }
 
   render() {
