2b72b09b88b11e6aaf56d71a77e7415db91cbe3b
evuz <jesus_jgb_45@hotmail.com>
2017-06-29 14:53:30 +0200

    Fix error when add electron module in renderer

--- scripts/config/webpack.config.dev.js
+++ scripts/config/webpack.config.dev.js
@@ -244,17 +244,11 @@ module.exports = {
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
   ],
-  // Some libraries import Node modules but don't use them in the browser.
-  // Tell Webpack to provide empty mocks for them so importing them works.
-  node: {
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
-  },
   // Turn off performance hints during development because we don't do any
   // splitting or minification in interest of speed. These warnings become
   // cumbersome.
   performance: {
     hints: false,
   },
+  target: 'electron-renderer'
 };
