32eda169a71e857765c44bbeb66e1fe0bd27621f
evuz <jesus_jgb_45@hotmail.com>
2017-06-30 11:23:01 +0200

    Components: add TaskItem Component

--- src/renderer/app/components/index.jsx
+++ src/renderer/app/components/index.jsx
@@ -1,9 +1,11 @@
 import AppComponent from './App/App';
 import HeaderComponent from './Header/Header';
 import TaskListComponent from './TaskList/TaskList';
+import TaskItemComponent from './TaskItem/TaskItem';
 
 export {
   AppComponent,
   HeaderComponent,
+  TaskItemComponent,
   TaskListComponent
 }
