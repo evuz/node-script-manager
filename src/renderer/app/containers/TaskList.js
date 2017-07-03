86ce10e8907f7159efbc331477aaae36b91df853
evuz <jesus_jgb_45@hotmail.com>
2017-07-03 13:03:18 +0200

    Send event run-script from renderer to main

--- src/renderer/app/containers/TaskList.js
+++ src/renderer/app/containers/TaskList.js
@@ -1,13 +1,21 @@
+import { ipcRenderer } from 'electron';
 import { connect } from 'react-redux';
 import { TaskListComponent } from '../components';
 import { changeScript } from '../reducers/app';
+
+function onTaskRun (newTask) {
+  return (dispatch) => {
+    ipcRenderer.send('run-script', newTask);
+    dispatch(changeScript(newTask));
+  }
+}
 
 const mapStateToProps = (state) => ({
     tasks: state.app.packageJson.scripts || []
 })
 
 const mapDispatchToProps = {
-    onTaskRun: changeScript
+    onTaskRun
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(TaskListComponent);
